126bab6a5a6c57dada7e49af06e978d98430d71a
Buck Doyle <buck.doyle@gmail.com>
2014-10-29 14:21:23 -0400

    Require admin login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  mount Upmin::Engine => '/admin'
+  authenticate :user, lambda { |u| u.admin? } do
+    mount Upmin::Engine => '/admin'
+  end
+
   root to: 'visitors#index'
   devise_for :users
 
