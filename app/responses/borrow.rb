1eb709735aa1f1ce8db87f016e7f8d7d7e0b81a7
Buck Doyle <buck.doyle@gmail.com>
2014-10-31 19:07:49 -0400

    Move NextBooking into Formatters namespace

--- app/responses/borrow.rb
+++ app/responses/borrow.rb
@@ -1,7 +1,7 @@
 module Responses
   class Borrow < AbstractResponse
     def body
-      "I am yours! #{afterspace_potential_content([@car.location_information, @car.lockbox_information, NextBookingFormatter.new(car: @from).output])}What is my odometer reading?"
+      "I am yours! #{afterspace_potential_content([@car.location_information, @car.lockbox_information, Formatters::NextBooking.new(car: @from).output])}What is my odometer reading?"
     end
   end
 end
