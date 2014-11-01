291e344471f538a57f280f7a9cfa1f63d89c0cab
Buck Doyle <buck.doyle@gmail.com>
2014-10-31 20:26:16 -0400

    Add shorter booking format when beginning and end are on the same day

--- app/formatters/booking.rb
+++ app/formatters/booking.rb
@@ -6,7 +6,11 @@ module Formatters
     end
 
     def format
-      "from #{@from.to_formatted_s} to #{@to.to_formatted_s}".gsub("  ", " ")
+      if @from.to_date == @to.to_date
+        "from #{@from.to_formatted_s.split.last} to #{@to.to_formatted_s.split.last} on #{@from.to_formatted_s.split.first}"
+      else
+        "from #{@from.to_formatted_s} to #{@to.to_formatted_s}".gsub("  ", " ")
+      end
     end
   end
 end
--- spec/formatters/booking_spec.rb
+++ spec/formatters/booking_spec.rb
@@ -19,7 +19,14 @@ describe Formatters::Booking do
   it 'uses full dates far in the future' do
     expect(format(
       "2015-06-01 8:00am",
-      "2015-06-01 9:00am"
-    )).to eq("from 2015-06-01 8:00AM to 2015-06-01 9:00AM")
+      "2015-06-02 9:00am"
+    )).to eq("from 2015-06-01 8:00AM to 2015-06-02 9:00AM")
+  end
+
+  it 'uses only one date when not spanning a day' do
+    expect(format(
+      "2015-06-01 8:00am",
+      "2015-06-01 9:00pm",
+    )).to eq("from 8:00AM to 9:00PM on 2015-06-01")
   end
 end
