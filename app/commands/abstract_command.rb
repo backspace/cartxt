3ea00c425dd366936a753d048cf1d06ab90afd02
Buck Doyle <buck.doyle@gmail.com>
2014-11-09 15:02:38 -0500

    Remove unused AbstractCommand methods

--- app/commands/abstract_command.rb
+++ app/commands/abstract_command.rb
@@ -8,14 +8,5 @@ module Commands
       @car = options[:car]
       @sharer = options[:sharer]
     end
-
-    protected
-    def append_response_to(to, body)
-      @responses.push Response.new(from: car, to: to, body: body)
-    end
-
-    def append_response(body)
-      append_response_to(sharer, body)
-    end
   end
 end
