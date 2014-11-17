59f676938f51a389265e5df4ba187644f18ca83a
Buck Doyle <buck.doyle@gmail.com>
2014-11-16 20:21:30 -0500

    Hardcode Capybara host and port to permit links in email

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -2,3 +2,6 @@ require 'capybara/poltergeist'
 
 Capybara.javascript_driver = :poltergeist
 Capybara.asset_host = 'http://localhost:3000'
+
+Capybara.server_host = Rails.application.config.action_mailer.default_url_options[:host].split(":").first
+Capybara.server_port = Rails.application.config.action_mailer.default_url_options[:host].split(":").last.to_i
