ca4d38a970017127e88a86d937c73401ea268104
Buck Doyle <b@chromatin.ca>
2015-02-05 18:40:04 -0500

    Block loading font stylesheet in testing
    Thanks to teampoltergeist/poltergeist#551

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -47,4 +47,8 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+  config.before(:each, js: true) do
+    page.driver.browser.url_blacklist = ["https://fonts.googleapis.com"]
+  end
 end
